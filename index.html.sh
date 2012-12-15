9ceb64254364f44ede8f8ed00cd795930c06d690
Thomas Levine <occurrence@thomaslevine.com>
2012-12-15 11:52:34 -0500

    add links to the csv

--- index.html.sh
+++ index.html.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+name=$(mktemp)
+twitter=$(mktemp)
+web=$(mktemp)
+
+# Add links.
+cut -d, -f1,2 soundsystem.csv > $name
+cut -d, -f3 soundsystem.csv | sed '2,$ s_^\(..*\)_<a href="https://twitter.com/\1/">\1</a>_' > $twitter
+cut -d, -f4 soundsystem.csv |sed '2,$ s_^\(..*\)_<a href="\1">\1</a>_' > $web
+
 echo '<html>
   <head>
     <style>
@@ -8,10 +17,10 @@ echo '<html>
   </head>
   <body>
     <pre>'
-cat soundsystem.csv
+paste -d, $name $twitter $web
 echo '    </pre>
   <script>
-  window.location.href = "/soundsystem.csv";
+  window.location.href = "soundsystem.csv";
   </script>
   </body>
 </html>'
