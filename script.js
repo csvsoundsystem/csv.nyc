5fde606334ad7a2e7a2d03ca41f6d555279a64eb
Thomas Levine <occurrence@thomaslevine.com>
2013-02-17 18:40:59 -0500

    Add sticker

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-var flash = function(i, bpm) {
-    // Lights to the beat (1874 rows/beats)
-    var j = (i + 1) % 4
-    document.body.style['background-color'] = ['#8F8', '#FF8', '#8FF', '#F8F'][j]
-    setTimeout(function() {
-        flash(j, bpm)
-    }, 60000 / bpm)
-}
-
-var go = function() {
-    // Start the flashing
-    if (!document.body.style['background-color']) {
-        flash(0, 80)
+(function(d){
+    var flash = function(i, bpm) {
+        // Lights to the beat (1874 rows/beats)
+        var j = (i + 1) % 4
+        d.body.style['background-color'] = ['#8F8', '#FF8', '#8FF', '#F8F'][j]
+        setTimeout(function() {
+            flash(j, bpm)
+        }, 60000 / bpm)
     }
-}
 
-document.body.addEventListener('click', go)
-document.body.addEventListener('touchend', go)
+    var go = function() {
+        // Start the flashing
+        if (!d.body.style['background-color']) {
+            flash(0, 80)
+        }
+    }
+
+    // Apply the flashing
+    d.body.addEventListener('click', go)
+    d.body.addEventListener('touchend', go)
+
+    // Add the sticker if it's not already there
+    if (!d.getElementById('csv-sticker')) {
+        d.body.innerHTML += '<img src="csvsoundsystem.png" alt="CSV Soundsystem" id="csv-sticker" style="position: absolute; right: 50; top: 10;" />'
+    }
+})(document)
