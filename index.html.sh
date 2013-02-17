6d9854b671e556c46e283f2dc303adb8fb5aa160
Thomas Levine <occurrence@thomaslevine.com>
2013-02-17 18:33:31 -0500

    target="_blank"

--- index.html.sh
+++ index.html.sh
@@ -6,8 +6,8 @@ web=$(mktemp)
 
 # Add links.
 cut -d, -f1,2 soundsystem.csv > $name
-cut -d, -f3 soundsystem.csv | sed '2,$ s_^\(..*\)_<a href="https://twitter.com/\1/">\1</a>_' > $twitter
-cut -d, -f4 soundsystem.csv |sed '2,$ s_^\(..*\)_<a href="\1">\1</a>_' > $web
+cut -d, -f3 soundsystem.csv | sed '2,$ s_^\(..*\)_<a target="\_blank" href="https://twitter.com/\1/">\1</a>_' > $twitter
+cut -d, -f4 soundsystem.csv |sed '2,$ s_^\(..*\)_<a target="\_blank" href="\1">\1</a>_' > $web
 
 echo '<html>
   <head>
