c3287d2d32dd511e18a95be77563a9156a24ca25
Brian Abelson <brianabelson@gmail.com>
2014-03-26 00:01:31 -0400

    Update csv.js

--- csv.js
+++ csv.js
@@ -1,8 +1,8 @@
-(function(d){
+(function(){
     var flash = function(i, bpm) {
         // Lights to the beat (1874 rows/beats)
         var j = (i + 1) % 4
-        d.body.style['background-color'] = ['#8F8', '#FF8', '#8FF', '#F8F'][j]
+        document.body.style['background-color'] = ['#8F8', '#FF8', '#8FF', '#F8F'][j]
         setTimeout(function() {
             flash(j, bpm)
         }, 60000 / bpm)
@@ -10,17 +10,15 @@
 
     var go = function() {
         // Start the flashing
-        if (!d.body.style['background-color']) {
+        if (!document.body.style['background-color']) {
             flash(0, 80)
         }
     }
 
     // Apply the flashing
-    d.body.addEventListener('click', go)
-    d.body.addEventListener('touchend', go)
+    document.body.addEventListener('click', go)
+    document.body.addEventListener('touchend', go)
 
-    // Add the sticker if it's not already there
-    if (!d.getElementById('csv-sticker')) {
-        d.body.innerHTML += '<a href="http://csvsoundsystem.com" target="_blank"><img src="http://csvsoundsystem.com/csvsoundsystem.png" alt="CSV Soundsystem" id="csv-sticker" style="position: absolute; right: 50px; top: 10px;" /></a>'
-    }
-})(document)
+    // Add the sticker
+    document.body.innerHTML += '<a href="http://csvsoundsystem.com" target="_blank"><img src="http://csvsoundsystem.com/csvsoundsystem.png" alt="CSV Soundsystem" id="csv-sticker" style="position: absolute; right: 50px; top: 10px;" /></a>'
+}).call(this)
